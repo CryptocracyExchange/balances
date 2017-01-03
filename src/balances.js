d8cbcae5c8ce5bd553d9721b9e40c52eb9df9ada
Harry Bellenie <harrybellenie@gmail.com>
2017-01-02 23:38:52 -0800

    handles when balance doesnt exist

--- src/balances.js
+++ src/balances.js
@@ -1,11 +1,7 @@
-// balances deepstream service
 const url = 'localhost:6020';
-
 const deepstream = require('deepstream.io-client-js');
-
 const client = deepstream(url).login();
 const Big = require('big.js');
-
 const testData = require('../test/testBalances');
 
 const initTestData = () => {
@@ -15,36 +11,39 @@ const initTestData = () => {
   const balance = client.record.getRecord(`balances/${userID}/${type}`);
   balance.set('amount', amount);
 };
+
 initTestData();
 
 module.exports.checkBalance = () => {
+  // need some kind of error handling
   client.event.subscribe('checkBalance', (data) => {
-    console.log('checkBalance listener', data);
-    const userID = data.userID;
-    const type = data.currency;
-    const balance = client.record.getRecord(`balances/${userID}/${type}`);
+    const balance = client.record.getRecord(`balances/${data.userID}/${data.currency}`);
     balance.whenReady(balance => {
       data.amount = balance.get('amount');
+      if (!data.amount) {
+        data.amount = 0;
+        balance.set('amount', data.amount);
+      }
       client.event.emit('returnBalance', data);
     });
     balance.discard();
   });
 };
 
-
 module.exports.updateBalance = () => {
   client.event.subscribe('updateBalance', (data) => {
-    console.log('updateBalance listener', data);
-    const userID = data.userID;
-    const type = data.currency;
-    const change = data.amount;
-    const balance = client.record.getRecord(`balances/${userID}/${type}`);
+    if (!data.update) {
+      console.log('no defined change');
+      data.update = '0';
+    }
+    const balance = client.record.getRecord(`balances/${data.userID}/${data.currency}`);
     balance.whenReady(balance => {
+      const change = data.update;
       const amount = Big(balance.get('amount')).plus(change);
-      // console.log('am', amount + change)
       balance.set('amount', amount);
-      data.amount = amount;
+      data.amount = Number(amount);
       client.event.emit('returnBalance', data);
     })
+    balance.discard();
   });
 };
