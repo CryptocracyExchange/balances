17a19ea7ff4644f3e4b8397a08478690fa7b2cd8
Harry Bellenie <harrybellenie@gmail.com>
2017-01-06 15:00:52 -0800

    preps for docker

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
 const events = require('./balances');
-const app = express();
 
+if (process.env.NODE_ENV !== 'prod') {
+  const express = require('express');
+  const path = require('path');
+  const morgan = require('morgan');
+  const app = express();
 
-app.use(morgan('combined'));
-app.use('/', express.static(path.join(__dirname, '../public')));
+  app.use(morgan('combined'));
+  app.use('/', express.static(path.join(__dirname, '../public')));
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
+}
 
 events.checkBalance();
 events.updateBalance();
